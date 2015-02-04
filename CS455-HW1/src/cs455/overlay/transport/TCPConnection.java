40e6118ebc3d16bc56d2b4bae2299a07f625876f
boyarkogc <boyarkogc@gmail.com>
2015-02-04 10:40:19 -0700

    Initial server and client code

--- CS455-HW1/src/cs455/overlay/transport/TCPConnection.java
+++ CS455-HW1/src/cs455/overlay/transport/TCPConnection.java
@@ -1,5 +1,13 @@
 package cs455.overlay.transport;
 
+import java.net.Socket;
+
+import cs455.overlay.node.Node;
+
 public class TCPConnection {
 
+	public TCPConnection(Node node, Socket socket) {
+		//this.receiver = new TCPReceiver(socket, node);
+		//this.sender = new TCPSender(socket);
+	}
 }
