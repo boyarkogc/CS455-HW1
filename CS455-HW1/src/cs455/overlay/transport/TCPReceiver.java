bb880d88fb4fa31e4e7139c5f488765295326f8f
boyarkogc <boyarkogc@gmail.com>
2015-02-05 12:32:50 -0700

    Copied code from assignment into TCPReceiver and TCPSender

--- CS455-HW1/src/cs455/overlay/transport/TCPReceiver.java
+++ CS455-HW1/src/cs455/overlay/transport/TCPReceiver.java
@@ -1,13 +1,34 @@
 package cs455.overlay.transport;
 
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.SocketException;
+
 public class TCPReceiver {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private Socket socket;
+	private DataInputStream din;
 
+	public TCPReceiver(Socket socket) throws IOException {
+		this.socket = socket;
+		din = new DataInputStream(socket.getInputStream());
 	}
-
+	
+	public void run() {
+		int dataLength;
+		while (socket != null) {
+			try {
+				dataLength = din.readInt();
+				byte[] data = new byte[dataLength];
+				din.readFully(data, 0, dataLength);
+			}catch(SocketException se) {
+				System.out.println(se.getMessage());
+				break;
+			}catch(IOException ioe) {
+				System.out.println(ioe.getMessage());
+				break;
+			}
+		}
+	}
 }
