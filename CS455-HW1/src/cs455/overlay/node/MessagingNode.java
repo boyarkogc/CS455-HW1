ce4a2bb9456a2a774b1655a562dea05b01d3422d
boyarkogc <boyarkogc@gmail.com>
2015-02-10 12:38:40 -0700

    created getters for ID, IPaddress, Port in MessagingNode class

--- CS455-HW1/src/cs455/overlay/node/MessagingNode.java
+++ CS455-HW1/src/cs455/overlay/node/MessagingNode.java
@@ -5,14 +5,27 @@ import java.io.*;
 
 import cs455.overlay.wireformats.Event;
 
-public class MessagingNode implements Node{
-	/*InetAddress nodeip;
+public class MessagingNode implements Node {
+	InetAddress nodeip;
 	int nodePort;
 	int ID;
-	*/
+	
+	public InetAddress getNodeip() {
+		return nodeip;
+	}
+
+	public int getNodePort() {
+		return nodePort;
+	}
+
+	public int getID() {
+		return ID;
+	}
+
 	public void onEvent(Event e) {
 		// TODO Auto-generated method stub
 		
 	}
+	
 	
 }
